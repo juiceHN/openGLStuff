054fc529d322dd5d4d84571699f8265ccfa16639
juiceHN <13341708+juiceHN@users.noreply.github.com>
2018-06-04 05:25:07 -0600

    Add files via upload

--- ogl.py
+++ ogl.py
@@ -6,21 +6,22 @@ window = pygame.display.set_mode((800, 800))
 done = False
 x, y = 50, 50
 while not done:
-	pygame.draw.line(screen, WHITE, (10, 10), (500, 500))
-	pygame.draw.line(screen, WHITE, (500, 500), 30, 1)
-    window.set_at((x, y), color)
+    r = rr.randint(0, 255)
+    g = rr.randint(0, 255)
+    b = rr.randint(0, 255)
+    color = (r, g, b)
+    pygame.draw.rect(window, color, [x, y, 50, 50], 5)
     pygame.display.flip()
-    window.fill((0,0,0))
+    window.fill((0, 0, 0))
     for event in pygame.event.get():
-    	if event.type == pygame.QUIT:
-    		done = True
-    	pressed = pygame.key.get_pressed()
-    	if pressed[pygame.K_RIGHT]:
-    		x += 10
-    	if pressed[pygame.K_LEFT]:
-    		x = x - 10
-    	if pressed[pygame.K_DOWN]:
-    		y = y - 10
-    	if pressed[pygame.K_UP]:
-    		y = y + 10
-
+        if event.type == pygame.QUIT:
+            done = True
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_RIGHT]:
+            x += 10
+        if pressed[pygame.K_LEFT]:
+            x = x - 10
+        if pressed[pygame.K_DOWN]:
+            y = y + 10
+        if pressed[pygame.K_UP]:
+            y = y - 10
